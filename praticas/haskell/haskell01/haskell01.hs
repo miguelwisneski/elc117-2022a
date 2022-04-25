1609a71676054cf9c42641def493a1af021c1424
miguelwisneski <85126042+miguelwisneski@users.noreply.github.com>
2022-04-25 12:47:45 -0300

    Update haskell01.hs

--- praticas/haskell/haskell01/haskell01.hs
+++ praticas/haskell/haskell01/haskell01.hs
@@ -3,6 +3,45 @@
 
 
 -- Parte 1
+--1
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x*x + y*y
 
+--2
+circleArea :: Float -> Float
+circleArea x = pi * x^2
+
+--3
+age :: Int -> Int -> Int
+age aN aA = aA - aN
+
+--4
+isElderly :: Int -> Bool
+isElderly i = i > 65
+
+--5
+htmlItem :: String -> String
+htmlItem x = "<li>" ++ x ++ "<li>"
+
+
+--6
+startsWithA :: String -> Bool
+startsWithA s = (head s) == 'A'
+
+--7
+isVerb :: String -> Bool
+isVerb x = (last x) == 's'
+
+--8
+isVowel :: Char -> Bool
+isVowel x = x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'
+
+--9
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads x z = (head x) == (head z)
+
+--10
+isVowel2 :: Char -> Bool
+isVowel2 x = elem 'x' "a" || elem 'x' "A" || elem 'x' "e" || elem 'x' "E" || elem 'x' "i" || elem 'x' "I" || elem 'o' "O" || elem 'x' "u" || elem 'x' "U" 
 
 -- Parte 2
